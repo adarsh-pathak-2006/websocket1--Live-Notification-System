96a89a13a09c2c450dd8cd7a2de449b7032721e4
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-21 22:01:39 +0530

    added the consumer file

--- core/consumers.py
+++ core/consumers.py
@@ -3,19 +3,30 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        user=self.scope["user"]
-        if user.is_authenticated():
-            print("connected")
-            await self.accept() 
+        self.user=self.scope["user"]
+        if self.user.is_authenticated: 
+            self.group_name=f"user_{self.user.id}"
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
+            print(f"{self.user.username} joined {self.group_name}")
+            await self.accept()
         else:
             await self.close()
                 
     async def receive(self, text_data):
         data=json.loads(text_data)
         message=data.get("message")
-        await self.send(json.dumps({'message':message}))
+        await self.send(text_data=json.dumps({'message':message}))
+
+    async def notification_message(self, event):
+        await self.send(text_data=json.dumps({
+            "title": event["title"],
+            "message": event["message"]
+        }))
 
     async def disconnect(self, close_code):
-        print(f"channel dissconnect code - {close_code}")
+        if hasattr(self, "group_name"):
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
+            print(f"{self.user.username} left {self.group_name}")
+            print(f"disconnected with code: {close_code}")
         
         
