e866aca5326678d325c3216d7497afff78e36263
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-21 16:51:17 +0530

    added authentication

--- core/consumers.py
+++ core/consumers.py
@@ -6,8 +6,10 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         user=self.scope["user"]
         if user.is_authenticated():
             print("connected")
-            await self.accept()     
-
+            await self.accept() 
+        else:
+            await self.close()
+                
     async def receive(self, text_data):
         data=json.loads(text_data)
         message=data.get("message")
