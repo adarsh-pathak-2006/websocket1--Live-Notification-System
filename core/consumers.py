524794359dd9610782a7f4798fd16a216344b660
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-21 16:19:42 +0530

    made echo level consumer

--- core/consumers.py
+++ core/consumers.py
@@ -1,20 +1,18 @@
-from channels.generic.websocket import WebsocketConsumer
 import json
+from core.models import Notification
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class NotificationConsumer(WebsocketConsumer):
+class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print("client connected")
+        print("connected")
         await self.accept()
-
-        await self.send(text_data=json.dump({'message':'welcome to the webscoket'}))
-
 
     async def receive(self, text_data):
         data=json.loads(text_data)
         message=data.get("message")
-        print(f"parsed data- {message}")
-
         await self.send(json.dumps({'message':message}))
 
-    async def disconnect(self, code):
-        print(f"connection terminated- {code}")
+    async def disconnect(self, close_code):
+        print(f"channel dissconnect code - {close_code}")
+        
+        
